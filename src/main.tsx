8cd49be8a1a0a23318e5ce79fe044abbed8e4bb0
jackfalc23 <jackfalconer289@gmail.com>
2026-09-01 16:37:01 -0700

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BeaconHub from './BeaconHub (1).jsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BeaconHub />
+  </React.StrictMode>
+);
